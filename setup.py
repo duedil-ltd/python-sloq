48a7be20ca5f768ef95f408d22befd73c05b1d9d
Paul Scott <paul.scotto@gmail.com>
2014-06-04 17:16:58 +0100

    Specify the long_description
    We include the demo_sloq module because it's probably going to be
    useful to have around and we require it to render the README.

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 
 VERSION = "0.5"
 REPO = "https://github.com/duedil-ltd/python-sloq"
+README = "README.rst"
+
+with open(README) as f:
+    long_description = f.read()
 
 setup(
     name="sloq",
@@ -12,9 +16,10 @@ setup(
     author_email="paul@duedil.com",
     url=REPO,
     download_url="%s/tarball/%s" % (REPO, VERSION),
-    py_modules=["sloq"],
+    py_modules=["sloq", "demo_sloq"],
     test_suite="test_sloq",
     license="MIT",
+    long_description=long_description,
     keywords="queue rate limit slow token bucket".split(),
     classifiers=[],
 )
